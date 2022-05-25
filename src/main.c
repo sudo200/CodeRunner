408e551f9db89ab5c61d2376a72cb55d2cbd7f3a
sudo200 <65343982+sudo200@users.noreply.github.com>
2022-05-25 09:59:41 +0200

    Use own flag parser library

--- src/main.c
+++ src/main.c
@@ -1,29 +1,30 @@
-#include <getopt.h>
+#include <getopts.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define VERSION "v1.0.2"
+#define VERSION "v1.1.0"
 
 static bool help_f = false;
 static bool version_f = false;
 
-static struct option options[] = {
-/* NAME		ARGS		FLAG			SHORT  */
-  {"help",	no_argument,	(int *)&help_f,		'h'},
-  {"version",	no_argument,	(int *)&version_f,	'v'},
+static const option_t options[] = {
+/* long		short	arg	flag			arg  */
+  {"help",	'h',	no_arg,	(int *)&help_f,		NULL },
+  {"version",	'v',	no_arg,	(int *)&version_f,	NULL },
 
-  {NULL,	no_argument,	NULL,			0}
+  {NULL,	'\0',	no_arg,	NULL,			NULL }
 };
 
 void usage(void);
 void version(void);
 
 int main(int argc, char** argv) {
-  int option_index = 0;
-  while(getopt_long_only(argc, argv, "", options, &option_index) != -1);
+  int option_index;
+  if(getopts(options, &option_index, argc, argv) != SUCCESS)
+    usage();
 
   if(help_f) // Show help page
     usage();
@@ -31,19 +32,19 @@ int main(int argc, char** argv) {
   if(version_f) // Show version
     version();
 
-  if(optind == argc) // No futher args -> Show help page
+  if(option_index == argc) // No futher args -> Show help page
     usage();
 
   size_t buffer_size = 0;
   
   // Get amount of buffer
-  for(size_t i = 1; i < argc; ++i)
+  for(size_t i = option_index; i < argc; ++i)
     buffer_size += (strlen(argv[i]) + 1) * sizeof(char);
 
   // Alloc buffer
   char* buffer_str = (char *)malloc(buffer_size);
 
-  for(size_t i = 1; i < argc; ++i) {
+  for(size_t i = option_index; i < argc; ++i) {
     strcat(buffer_str, argv[i]);
     if((i + 1) < argc) strcat(buffer_str, " ");
   }
